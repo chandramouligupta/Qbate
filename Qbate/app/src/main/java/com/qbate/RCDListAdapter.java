e07cd4f9a2b61aee5fe46ffdbc019543a2d57188
Chandramouli <chandramouligupta50@gmail.com>
2019-04-17 17:11:36 +0530

    Replies Functionality Integrated.

--- Qbate/app/src/main/java/com/qbate/RCDListAdapter.java
+++ Qbate/app/src/main/java/com/qbate/RCDListAdapter.java
@@ -1,13 +1,9 @@
 package com.qbate;
 
 import android.content.Context;
-import android.content.Intent;
-import android.content.res.ColorStateList;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Color;
-import android.graphics.Typeface;
-import android.net.Uri;
 import android.os.AsyncTask;
 import android.preference.PreferenceManager;
 import android.support.annotation.NonNull;
@@ -27,47 +23,34 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
-import org.w3c.dom.Comment;
-import org.xml.sax.InputSource;
-import org.xml.sax.XMLReader;
-
-import java.io.IOException;
 import java.io.InputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 
-import javax.xml.parsers.SAXParser;
-import javax.xml.parsers.SAXParserFactory;
-
-public class CommentListAdapter extends BaseAdapter {
-
+public class RCDListAdapter extends BaseAdapter { // ReplyCommentDisplayListAdapter class
+    
     private Context context;
-    private ArrayList<CommentItem> commentsItemList;
+    private ArrayList<CommentItem> replyItemArrayList;
 
-    public CommentListAdapter(Context context, ArrayList<CommentItem> commentsItemList) {
+    public RCDListAdapter(Context context, ArrayList<CommentItem> replyItemArrayList) {
         this.context = context;
-        this.commentsItemList = commentsItemList;
+        this.replyItemArrayList = replyItemArrayList;
     }
 
     @Override
     public int getCount() {
-        return commentsItemList.size();
+        return replyItemArrayList.size();
     }
 
     @Override
-    public Object getItem(int pos) {
-        return commentsItemList.get(pos);
+    public Object getItem(int position) {
+        return replyItemArrayList.get(position);
     }
 
     @Override
-    public long getItemId(int pos) {
-        return pos;
+    public long getItemId(int position) {
+        return position;
     }
 
     @Override
@@ -76,20 +59,21 @@ public class CommentListAdapter extends BaseAdapter {
         final DatabaseReference likesTableRef = FirebaseDatabase.getInstance().getReference("likes");
         final DatabaseReference dislikesTableRef = FirebaseDatabase.getInstance().getReference("dislikes");
         final DatabaseReference irrelevantTableRef = FirebaseDatabase.getInstance().getReference("irrelevants");
-        final Query checkIfLiked = FirebaseDatabase.getInstance().getReference("likes")
-                .child(commentsItemList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
-        final Query checkIfDisliked = FirebaseDatabase.getInstance().getReference("dislikes")
-                .child(commentsItemList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
-        final Query checkIfIrrelevantClicked = FirebaseDatabase.getInstance().getReference("irrelevants")
-                .child(commentsItemList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
 
-        final String email = commentsItemList.get(position).getEmail();
+        Log.d("testing27",replyItemArrayList.get(position).getCommentId());
+
+        final Query checkIfLiked = FirebaseDatabase.getInstance().getReference("likes")
+                .child(replyItemArrayList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
+        final Query checkIfDisliked = FirebaseDatabase.getInstance().getReference("dislikes")
+                .child(replyItemArrayList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
+        final Query checkIfIrrelevantClicked = FirebaseDatabase.getInstance().getReference("irrelevants")
+                .child(replyItemArrayList.get(position).getCommentId()).orderByKey().equalTo(creatorId);
+
+        final String email = replyItemArrayList.get(position).getEmail();
         Log.d("testing","UID:" + email);
         final TextView commentTitle;
         TextView userName;
         TextView dateTime;
-        final TextView replyText;
-        final TextView viewAllReply;
         final TextView upVoteTextView;
         final TextView downVoteTextView;
         final TextView irrelevantTextView;
@@ -100,40 +84,11 @@ public class CommentListAdapter extends BaseAdapter {
             v = View.inflate(context, R.layout.my_message, null);
             commentTitle = v.findViewById(R.id.my_message_body);
             dateTime = v.findViewById(R.id.my_comment_time);
-
-            viewAllReply = v.findViewById(R.id.view_all_reply_my_message);
-
-            FirebaseDatabase.getInstance().getReference("replies")
-                    .orderByKey().equalTo(commentsItemList.get(position).getCommentId())
-                    .addValueEventListener(new ValueEventListener() {
-                        @Override
-                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                            if(dataSnapshot == null){
-                                //No Replies till now
-                            }else{
-                                viewAllReply.setVisibility(View.VISIBLE);
-                            }
-                        }
-                        @Override
-                        public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                        }
-                    });
-
-            viewAllReply.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context,ReplyCommentDisplay.class);
-                    intent.putExtra("topicId",commentsItemList.get(position).getTopicId());
-                    intent.putExtra("categoryId",commentsItemList.get(position).getCategoryId());
-                    intent.putExtra("commentId",commentsItemList.get(position).getCommentId());
-                    context.startActivity(intent);
-                }
-            });
-
-            commentTitle.setText(commentsItemList.get(position).getCommentTitle());
+            TextView  viewAllReplyMyMessage = v.findViewById(R.id.view_all_reply_my_message);
+            viewAllReplyMyMessage.setVisibility(View.GONE);
+            commentTitle.setText(replyItemArrayList.get(position).getCommentTitle());
             SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yy' 'hh:mm a", Locale.US);
-            dateTime.setText(sdf.format(commentsItemList.get(position).getTimestamp()));
+            dateTime.setText(sdf.format(replyItemArrayList.get(position).getTimestamp()));
             upVoteTextView = v.findViewById(R.id.my_upvote);
             downVoteTextView = v.findViewById(R.id.my_downvote);
             irrelevantTextView = v.findViewById(R.id.my_irrelevant);
@@ -142,52 +97,32 @@ public class CommentListAdapter extends BaseAdapter {
         else {
             v = View.inflate(context, R.layout.their_message, null);
             ImageView avatar = v.findViewById(R.id.avatar);
-
-            replyText = v.findViewById(R.id.add_reply_text_their_message);
-            viewAllReply = v.findViewById(R.id.view_all_reply_their_message);
-
-            replyText.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context,ReplyCommentDisplay.class);
-                    intent.putExtra("topicId",commentsItemList.get(position).getTopicId());
-                    intent.putExtra("categoryId",commentsItemList.get(position).getCategoryId());
-                    intent.putExtra("commentId",commentsItemList.get(position).getCommentId());
-                    context.startActivity(intent);
-                }
-            });
-
-            viewAllReply.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    Intent intent = new Intent(context,ReplyCommentDisplay.class);
-                    intent.putExtra("topicId",commentsItemList.get(position).getTopicId());
-                    intent.putExtra("categoryId",commentsItemList.get(position).getCategoryId());
-                    intent.putExtra("commentId",commentsItemList.get(position).getCommentId());
-                    context.startActivity(intent);
-                }
-            });
-
             final TextView like = v.findViewById(R.id.thumbs_up);
             final TextView dislike = v.findViewById(R.id.thumbs_down);
             final TextView irrelevant = v.findViewById(R.id.irrelevant);
+
+            final TextView replyTextView = v.findViewById(R.id.add_reply_text_their_message);
+            final TextView viewAllReplyTextTheirMessage = v.findViewById(R.id.view_all_reply_their_message);
+            replyTextView.setVisibility(View.GONE);
+            viewAllReplyTextTheirMessage.setVisibility(View.GONE);
+
             commentTitle = v.findViewById(R.id.their_message_body);
             userName = v.findViewById(R.id.uname);
             dateTime = v.findViewById(R.id.their_comment_time);
             upVoteTextView = v.findViewById(R.id.their_upvote);
             downVoteTextView = v.findViewById(R.id.their_downvote);
             irrelevantTextView = v.findViewById(R.id.their_irrelevant);
-            commentTitle.setText(commentsItemList.get(position).getCommentTitle());
-            String photoUrl = commentsItemList.get(position).getPhotoUrl();
+            commentTitle.setText(replyItemArrayList.get(position).getCommentTitle());
+            String photoUrl = replyItemArrayList.get(position).getPhotoUrl();
             if(!photoUrl.equalsIgnoreCase("NO_PROFILE_PIC")) {
-                Log.d("testing",commentsItemList.get(position).getPhotoUrl());
+                Log.d("testing",replyItemArrayList.get(position).getPhotoUrl());
 
-                new DownloadImageTask((ImageView) v.findViewById(R.id.avatar))
+                new RCDListAdapter.DownloadImageTask((ImageView) v.findViewById(R.id.avatar))
                         .execute(photoUrl);
             }
-            userName.setText(commentsItemList.get(position).getUsername());
+            userName.setText(replyItemArrayList.get(position).getUsername());
             SimpleDateFormat sdf = new SimpleDateFormat("dd-MMM-yy' 'hh:mm a", Locale.US);
-            dateTime.setText(sdf.format(commentsItemList.get(position).getTimestamp()));
+            dateTime.setText(sdf.format(replyItemArrayList.get(position).getTimestamp()));
 
             //Setting Textview Liked,Disliked,Irrelevant
             checkIfLiked.addValueEventListener(new ValueEventListener() {
@@ -242,7 +177,7 @@ public class CommentListAdapter extends BaseAdapter {
                 public void onClick(View view) {
                     //i.e inserting an item in dislike table with key as current USERKEY
                     ILDItem obj = new ILDItem(googleSignInAccount.getEmail());
-                    dislikesTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).setValue(obj);
+                    dislikesTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).setValue(obj);
                     like.setTextColor(Color.parseColor("#4267b2"));
                     like.setText("Unliked");
                     dislike.setVisibility(View.INVISIBLE);
@@ -254,7 +189,7 @@ public class CommentListAdapter extends BaseAdapter {
                 public void onClick(View view) {
                     //i.e insetring an item in irrelevant table with key as current USERKEY
                     ILDItem obj = new ILDItem(googleSignInAccount.getEmail());
-                    irrelevantTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).setValue(obj);
+                    irrelevantTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).setValue(obj);
                     like.setTextColor(Color.parseColor("#4267b2"));
                     like.setText("Irrelevant");
                     dislike.setVisibility(View.INVISIBLE);
@@ -269,21 +204,21 @@ public class CommentListAdapter extends BaseAdapter {
                     if("Unliked".equalsIgnoreCase(like.getText().toString())){
                         //i.e User has already disliked this but now he changed his mind and want
                         // set it back to neutral
-                        dislikesTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).removeValue();
+                        dislikesTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).removeValue();
                         like.setTextColor(Color.parseColor("#231F20"));
                         like.setText("Like");
                         dislike.setVisibility(View.VISIBLE);
                         irrelevant.setVisibility(View.VISIBLE);
                     }else if("Irrelevant".equalsIgnoreCase(like.getText().toString())){
                         //i.e user has  marked it irrelevant but now making it neutral
-                        irrelevantTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).removeValue();
+                        irrelevantTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).removeValue();
                         like.setTextColor(Color.parseColor("#231F20"));
                         like.setText("Like");
                         dislike.setVisibility(View.VISIBLE);
                         irrelevant.setVisibility(View.VISIBLE);
                     }else if("Liked".equalsIgnoreCase(like.getText().toString())) {
                         // i.e User has already Liked it but now he maiy be setting it to neutral
-                        likesTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).removeValue();
+                        likesTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).removeValue();
                         like.setTextColor(Color.parseColor("#231F20"));
                         like.setText("Like");
                         dislike.setVisibility(View.VISIBLE);
@@ -293,7 +228,7 @@ public class CommentListAdapter extends BaseAdapter {
                         ILDItem obj = new ILDItem(googleSignInAccount.getEmail());
                         like.setText("Liked");
                         like.setTextColor(Color.parseColor("#4267b2"));
-                        likesTableRef.child(commentsItemList.get(position).getCommentId()).child(creatorId).setValue(obj);
+                        likesTableRef.child(replyItemArrayList.get(position).getCommentId()).child(creatorId).setValue(obj);
                         dislike.setVisibility(View.INVISIBLE);
                         irrelevant.setVisibility(View.INVISIBLE);
                     }
@@ -303,7 +238,7 @@ public class CommentListAdapter extends BaseAdapter {
 
         //Displaying Upvote,Downvote,Irrelevant Counts
 
-        likesTableRef.child(commentsItemList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
+        likesTableRef.child(replyItemArrayList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //here dataSnapShot object will define how many likes does that post has
@@ -323,7 +258,7 @@ public class CommentListAdapter extends BaseAdapter {
             }
         });
 
-        dislikesTableRef.child(commentsItemList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
+        dislikesTableRef.child(replyItemArrayList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //here dataSnapShot object will define how many likes does that post has
@@ -343,7 +278,7 @@ public class CommentListAdapter extends BaseAdapter {
             }
         });
 
-        irrelevantTableRef.child(commentsItemList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
+        irrelevantTableRef.child(replyItemArrayList.get(position).getCommentId()).addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
                 //here dataSnapShot object will define how many likes does that post has
@@ -366,10 +301,9 @@ public class CommentListAdapter extends BaseAdapter {
 
 
         //saving product id to the tag
-        v.setTag(commentsItemList.get(position).getCommentId());
+        v.setTag(replyItemArrayList.get(position).getCommentId());
         return v;
     }
-
     private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
         ImageView bmImage;
 
